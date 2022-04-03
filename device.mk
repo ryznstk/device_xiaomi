ff705b77beba0176e06bea8aa6b724a9224893f8
AdarshGrewal <adarshgrewal@gmail.com>
2022-04-03 19:18:28 +0530

    peridot: Include MiuiCamera if exists
    Change-Id: Iee675b4924cb2be7ac2d36c7737b06edc9cadfc9
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>
    Signed-off-by: Shikhar <Krtonia@gmail.com>

--- device.mk
+++ device.mk
@@ -23,6 +23,9 @@ $(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk
 $(call soong_config_set,rfs,mpss_firmware_symlink_target,modem_firmware)
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# Call the MiuiCamera setup
+$(call inherit-product-if-exists, device/xiaomi/peridot-miuicamera/device.mk)
+
 # Viper4AndroidFX
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
