24fa28bff0a880fc84ab46446ceb9c2944b057a1
basamaryan <basam.aryan@gmail.com>
2024-05-29 02:11:18 +0530

    peridot: Switch to common libqti-perfd-client and power-libperfmgr
    Also import powerhint from oneplus-sm8650 tree
    
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -258,7 +258,6 @@ SPAMMY_LOG_TAGS := \
     MiStcImpl \
     SDM \
     SRE \
-    android.hardware.power-service-qti \
     libsensor-boledalgo \
     libsensor-parseRGB \
     libsensor-qshcalapi \
@@ -342,10 +341,11 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/pineapple/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QMI
 PRODUCT_PACKAGES += \
@@ -366,7 +366,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi \
-    vendor/qcom/opensource/usb/etc
+    vendor/qcom/opensource/usb/etc \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/qcom-caf/common/libqti-perfd-client \
+    hardware/lineage/interfaces/power-libperfmgr
 
 # Sensors
 PRODUCT_PACKAGES += \
