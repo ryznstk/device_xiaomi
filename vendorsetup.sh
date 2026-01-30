63ab8890d99f7201388abc52bc40ef56a5d12bed
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-01-30 10:56:05 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -49,7 +49,7 @@ git clone https://github.com/droidcore/packages_apps_GameBar.git packages/apps/G
 # LMO
 echo "Cloning LMOfreeroam tree..."
 rm -rf packages_apps_LMOFreeform
-git clone https://github.com/ryznstk/packages_apps_LMOFreeform.git packages_apps_LMOFreeform
+git clone https://github.com/ryznstk/packages_apps_LMOFreeform.git packages/apps/LMOFreeform
 
 rm -rf system/sepolicy
 git clone -b bq2 https://github.com/ryznstk/system_sepolicy system/sepolicy
