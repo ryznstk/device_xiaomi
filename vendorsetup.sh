d679db028025dc1733421f23551437cc9230a318
ryznstk <agusmhrd1k4@gmail.com>
2026-02-02 06:42:03 +0700

    hmm

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,13 +8,19 @@ git clone -b lineage-23.2 https://gitlab.com/blu96/vendor-xiaomi-peridot-qpr-2.g
 # Kernel source (fresh clone)
 echo "Cloning kernel source tree..."
 rm -rf kernel/xiaomi/sm8635
-git clone -b nonksu --depth 1 https://github.com/ryznstk/android_kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
+git clone -b test --depth 1 https://github.com/ryznstk/android_kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
 
 rm -rf kernel/xiaomi/sm8635-modules
 git clone -b lineage-23.0 --depth 1 https://github.com/ryznstk/android_kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
 
 rm -rf kernel/xiaomi/sm8635-devicetrees
 git clone -b lineage-23.2 --depth 1 https://github.com/peridot-dev/android_kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
+
+# Setup KernelSU-Next
+echo "Setting up KernelSU-Next..."
+pushd kernel/xiaomi/sm8635
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -
+popd
 
 # Hardware xiaomi (fresh clone)
 echo "Cloning hardware xiaomi source..."
